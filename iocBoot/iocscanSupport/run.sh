eccfd390e0be21f1f54515ea90af178aa1839b87
apanna <apanna1236@gmail.com>
2016-08-31 13:20:44 -0400

    FIX: comment caputRecorder startup script

--- iocBoot/iocscanSupport/run.sh
+++ iocBoot/iocscanSupport/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 chmod +x st.cmd
-sh start_putrecorder.sh
+#sh start_putrecorder.sh
 procServ --allow -n "SCAN" -p pid.txt -L log.txt --logstamp -i ^D^C 2000 ../../bin/$EPICS_HOST_ARCH/scanSupport st.cmd
 sleep 10
